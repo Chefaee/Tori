fe3b32ccfd028b89300e565b5cec3ded5d946479
Spottedstorm23 <cv.stokowsky@gmx.de>
2024-05-23 10:38:57 +0200

    feature: make gps more reliable

--- app/src/main/java/com/htwpeeps/tori/MapActivity.java
+++ app/src/main/java/com/htwpeeps/tori/MapActivity.java
@@ -7,23 +7,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.provider.Settings;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
+import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
@@ -32,16 +35,22 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
  * This Class holds all necessary functionalities for the second screen with the map.
  */
 public class MapActivity extends AppCompatActivity {
+    public static final int LOCATION_UPDATE_INTERVAL = 5;
     //necessary for the map
     private MapView map;
     private IMapController mapController;
     private MyLocationNewOverlay mLocationOverlay;
     private static final int PERMISSION_REQUEST_CODE = 1;
 
-    LocationManager locationManager;
     // placeholder values for starting point, for now they are set to frauenkirche in dresden
     double latitude = 51.051873;
     double longitude = 13.741522;
+
+    //API from Google for location
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,30 +61,19 @@ public class MapActivity extends AppCompatActivity {
         endButton.setOnClickListener(v -> startActivity(new Intent(this, MainActivity.class)));
 
 
-        //TODO refactor permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            String[] permissions = {Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
-        }
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000 * LOCATION_UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
 
-        // get location via gps
-        //TODO make this reliable
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            //Asking to turn on GPS
-            startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-        }
-        Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (locationGPS != null) {
-            latitude = locationGPS.getLatitude();
-            longitude = locationGPS.getLongitude();
-        }
+        initializeMap();
 
-        //load/initialize the osmdroid configuration, this can be done
+        // update longitude and latitude
+        updateGPS();
+
+    }
+
+    private void initializeMap() {
         Context ctx = getApplicationContext();
         //TODO refactor deprication
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
@@ -107,6 +105,34 @@ public class MapActivity extends AppCompatActivity {
         this.mLocationOverlay.enableMyLocation();
         this.mLocationOverlay.setDrawAccuracyEnabled(true);
         map.getOverlays().add(this.mLocationOverlay);
+    }
+
+    private void updateGPS(){
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapActivity.this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            updateMap(mapController);
+        });
+
+    }
+
+    private void updateMap(IMapController mapController){
+        GeoPoint point = new GeoPoint(latitude, longitude);
+        mapController.setCenter(point);
+        //this moves the map to actually center the start point
+        mapController.animateTo(point);
     }
 
 }
