2ccc83e9af7357c394ae333c23ad1e4e4f353cf0
Spottedstorm23 <cv.stokowsky@gmx.de>
2024-06-04 18:35:13 +0200

    feature: update location request when paused feature: stop tracking when map is closed feature: style polyline on map feature: add tracking information on start screen feature: give activity from dropdown to map chore: add documentation

--- app/src/main/java/com/htwpeeps/tori/MainActivity.java
+++ app/src/main/java/com/htwpeeps/tori/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -12,18 +14,31 @@ import android.widget.Spinner;
  * Activity class that holds the functionalities for the first screen.
  * It supplies the dropdown values for possible activities on a field as well as the change to the map activity.
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    String activeDropDownString = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button startButton = (Button) findViewById(R.id.start_button);
-        //todo give dropdown value to map for api
-        startButton.setOnClickListener(v -> startActivity(new Intent(this, MapActivity.class)));
-
+        // handle everything regarding the activity dropdown
+        Spinner activityDropdown = findViewById(R.id.activity_dropdown);
+        // listener for dropdown
+        activityDropdown.setOnItemSelectedListener(this);
         setDropdownValues();
+
+        //set standard value if nothing selected to the first available item
+        activeDropDownString = activityDropdown.getItemAtPosition(0).toString();
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MapActivity.class);
+            intent.putExtra("activity", activeDropDownString);
+            startActivity(intent);
+        });
+
         ApiCall apiCall = new ApiCall(
                 51.89700414986401,
                 -1.368870281720362,
@@ -34,11 +49,11 @@ public class MainActivity extends AppCompatActivity {
                     // Here you girls can do whatever frontend-stuff you want with fieldIndex, for example:
                     if (result.fieldIndex != null) {
                         System.out.println(result.fieldIndex + ", " + result.responseCode);
-                    } else if (result.responseCode == null ) {
+                    } else if (result.responseCode == null) {
                         System.out.println("Cant establish network connection to server :(");
                     } else {
                         System.out.println("There are problems with the server." +
-                            "Http-Response Code " + result.responseCode);
+                                "Http-Response Code " + result.responseCode);
                     }
                 });
         apiCall.execute();
@@ -49,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
      * This methods sets the values that are given in the activity dropdown.
      * To change the items add, update or remove an item in the {@link com.htwpeeps.tori.R.array#activity_array}.
      * Remember to update in all translations as well
+     * It would be possible to read all possible activities from another source (a database) as long as it can be accessed as an array of strings.
      */
     private void setDropdownValues() {
         //access spinner from xml
@@ -60,5 +76,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, activityItems);
         //set the spinners adapter to the previously created one.
         activityDropdown.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        activeDropDownString = parent.getItemAtPosition(position).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+
     }
 }
--- app/src/main/java/com/htwpeeps/tori/MapActivity.java
+++ app/src/main/java/com/htwpeeps/tori/MapActivity.java
@@ -1,41 +1,46 @@
 package com.htwpeeps.tori;
 
-import static android.util.Log.println;
-
+// standard android import
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
+
+// ui imports
 import android.widget.Button;
 import android.widget.TextView;
 
+// androidx imports
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
+import androidx.preference.PreferenceManager;
 
+// location api from google
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Granularity;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.location.Priority;
 
+//open street map imports
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
-import org.osmdroid.views.MapController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
@@ -43,48 +48,48 @@ import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
  * This Class holds all necessary functionalities for the second screen with the map.
+ * This includes requesting permissions for location tracking, the tracking itself as well as the drawing of the map.
  */
 public class MapActivity extends AppCompatActivity {
 
     //necessary for the map
+    //the map itself
     private MapView map;
+    // controller to determine which part is currently shown
     private IMapController mapController;
+    //overlay where the position as well as the line is drawn
     private MyLocationNewOverlay mLocationOverlay;
+    // lin eof the last points
     private Polyline polylinePath;
+    //necessary if permissions have to be asked for
     private static final int PERMISSION_REQUEST_CODE = 1;
 
     //API from Google for location
     FusedLocationProviderClient fusedLocationProviderClient;
     LocationRequest locationRequest;
+    // these are later multiplied with 1000 for millisec
     public static final int LOCATION_UPDATE_INTERVAL = 5;
     public static final int LOCATION_UPDATE_PAUSED_INTERVAL = 10;
 
-    // tracking
-    /*
-     last point
-     starttime since no change
-     is paused
-     is field
-     fieldchange?
-
-     moved while paused?
-     */
-    // placeholder values used for starting point, for now they are set to frauenkirche in dresden
+    // location
     LocationCallback locationCallBack;
+    // placeholder values used for starting point, for now they are set to frauenkirche in dresden
     GeoPoint lastKnownPoint = new GeoPoint(51.051873, 13.741522);
 
     List<GeoPoint> last10Points = new ArrayList<>();
 
     boolean trackingIsPaused = false;
+
+    //todo remove if not necessary
     boolean trackingIsInField = false;
     boolean changedIsInField = false;
     boolean movedWhilePaused = false;
 
+    //todo get this from main
     private String activity = "plowing";
 
 
@@ -100,36 +105,75 @@ public class MapActivity extends AppCompatActivity {
         Button endButton = (Button) findViewById(R.id.end_button);
         endButton.setOnClickListener(v -> startActivity(new Intent(this, MainActivity.class)));
 
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(1000 * LOCATION_UPDATE_INTERVAL);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            System.out.println(extras.getString("activity"));
+        }
+
+        locationRequest =
+                new LocationRequest.Builder(
+                        Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+                        1000 * LOCATION_UPDATE_INTERVAL
+                ).setGranularity(Granularity.GRANULARITY_FINE)
+                        .setMinUpdateIntervalMillis(1000 * LOCATION_UPDATE_INTERVAL).build();
 
         Button pauseButton = (Button) findViewById(R.id.pause_button);
         pauseButton.setOnClickListener(v -> {
-            if (!trackingIsPaused){
+            if (!trackingIsPaused) {
                 trackingIsPaused = true;
+
+                //creates a new locationRequest with a longer interval between location updates, as paused location should not change
+                locationRequest =
+                        new LocationRequest.Builder(
+                                Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+                                1000 * LOCATION_UPDATE_PAUSED_INTERVAL
+                        ).setGranularity(Granularity.GRANULARITY_FINE)
+                                .setMinUpdateIntervalMillis(1000 * LOCATION_UPDATE_PAUSED_INTERVAL).build();
+
+                //called to update the request
+                tracking();
+
+                //ui updates
                 status.setText(getString(R.string.pause_status));
-                //TODO check why interval is not updateing
-                locationRequest.setInterval(1000 * LOCATION_UPDATE_PAUSED_INTERVAL);
                 pauseButton.setText(getString(R.string.cont_button));
-            }
-            else {
+            } else {
                 trackingIsPaused = false;
+                // recreates the original locationRequest with the standard interval for tracking
+                locationRequest =
+                        new LocationRequest.Builder(
+                                Priority.PRIORITY_BALANCED_POWER_ACCURACY,
+                                1000 * LOCATION_UPDATE_INTERVAL
+                        ).setGranularity(Granularity.GRANULARITY_FINE)
+                                .setMinUpdateIntervalMillis(1000 * LOCATION_UPDATE_INTERVAL).build();
+
+                //called to update the request
+                tracking();
+
+                //ui updates
                 status.setText("");
-                locationRequest.setInterval(1000 * LOCATION_UPDATE_INTERVAL);
-                pauseButton.setText( getString(R.string.pause_button));
+                pauseButton.setText(getString(R.string.pause_button));
 
             }
 
         });
 
 
-
         initializeMap();
+
+        /* This is called everytime a location request is made and returned a result.
+         * As long as a valid location is returned all functions to update values and ui as well as the api are called.
+         */
         locationCallBack = new LocationCallback() {
             @Override
             public void onLocationResult(@NonNull LocationResult locationResult) {
                 super.onLocationResult(locationResult);
+
+                System.out.println(Instant.now().getEpochSecond());
+
+                if (locationResult.getLastLocation() == null) {
+                    return;
+                }
 
                 UpdatePointValues(locationResult.getLastLocation());
                 updateMap();
@@ -142,15 +186,28 @@ public class MapActivity extends AppCompatActivity {
         tracking();
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // for now the tracking is stopped when the activity ends
+        stopTracking();
+    }
+
+    /**
+     * Creates an osm-map in the view.
+     * Therefore the view element is accessed and filed with map tiles, any map controls are disabled.
+     * Then the map is set to the starting point, which is most of the times the placeholder from the start.
+     * Everything is set to a fixed zoom, all necessary layers (for displaying lines and location) are created, the icon is replaced with a custom icon and the line is defined.
+     */
     private void initializeMap() {
         Context ctx = getApplicationContext();
-        //TODO refactor deprication
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         //setting this before the layout is inflated is a good idea
         //it 'should' ensure that the map has a writable location for the map cache, even without permissions
         //if no tiles are displayed, you can try overriding the cache path using Configuration.getInstance().setCachePath
         //see also StorageUtils
         //note, the load method also sets the HTTP User Agent to your application's package name, abusing osm's tile servers will get you banned based on this string
+
         map = findViewById(R.id.osmmap);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
@@ -177,23 +234,35 @@ public class MapActivity extends AppCompatActivity {
         if (currentDraw != null) {
             currentIcon = ((BitmapDrawable) currentDraw).getBitmap();
             this.mLocationOverlay.setPersonIcon(currentIcon);
-            this.mLocationOverlay.setPersonAnchor(0.5f,0.5f);
+            this.mLocationOverlay.setPersonAnchor(0.5f, 0.5f);
             this.mLocationOverlay.setDirectionIcon(currentIcon);
-            this.mLocationOverlay.setDirectionAnchor(0.5f,0.5f);
+            this.mLocationOverlay.setDirectionAnchor(0.5f, 0.5f);
         }
 
+        // add layer to display the location
         this.mLocationOverlay.enableMyLocation();
         this.mLocationOverlay.setDrawAccuracyEnabled(true);
         map.getOverlays().add(this.mLocationOverlay);
 
-        // adds the layer needed to draw the last 10 points as a line
+        // to style the line, one has to access the paint and update the values on that
         polylinePath = new Polyline();
-        polylinePath.setColor(Color.parseColor("#5ce1e6"));
-        polylinePath.setWidth(4.5f);
+        Paint polypaint = polylinePath.getOutlinePaint();
+        polypaint.setColor(Color.parseColor("#5ce1e6"));
+        polypaint.setStrokeWidth(6.5f);
+        polypaint.setPathEffect(new DashPathEffect(new float[]{7.5f, 7.5f}, 0));
+
+        // adds the line layer
         map.getOverlays().add(polylinePath);
 
     }
 
+    /**
+     * Upon start of tracking, the list of the last known points is initialized at the current location.
+     * It also initializes the value for the lastKnownPoint
+     * This should prevent an unwanted line upon the update to the first location
+     *
+     * @param location current location returned by the location request
+     */
     private void initializeLastPointList(Location location) {
         lastKnownPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
 
@@ -223,14 +292,19 @@ public class MapActivity extends AppCompatActivity {
 
 
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
+            System.out.println(Instant.now().getEpochSecond());
             UpdatePointValues(location);
             updateMap();
             drawPathLine();
-            sendApiCall();
+            //sendApiCall();
         });
 
     }
 
+    /**
+     * The first location update is different to all following as the function to initialize the lastPointList must be called.
+     * Afterwards the map is updated and the api called
+     */
     private void updateFirstGPS() {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapActivity.this);
@@ -247,41 +321,76 @@ public class MapActivity extends AppCompatActivity {
         fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, location -> {
             initializeLastPointList(location);
             updateMap();
-            drawPathLine();
             sendApiCall();
         });
 
     }
 
+    /**
+     * This function centers the map on the current position and if it deems it necessary moves the map tiles accordingly.
+     */
     private void updateMap() {
         mapController.setCenter(lastKnownPoint);
         //this moves the map to actually center the start point
         mapController.animateTo(lastKnownPoint);
+        System.out.println("Map:" + Instant.now().getEpochSecond());
     }
 
+    /**
+     * Updates the line on the map.
+     * There are always only 10 points in use, but the length of the line might differ depending on the speed the device is moving.
+     */
     private void drawPathLine() {
         polylinePath.setPoints(last10Points);
     }
 
+    /**
+     * The current location is saved and added at the end of the pointsList. The then oldest point is removed.
+     *
+     * @param location current location returned by location request
+     */
     private void UpdatePointValues(Location location) {
         lastKnownPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
         last10Points.remove(0);
         last10Points.add(9, lastKnownPoint);
     }
 
+    /**
+     * This function checks for permissions before setting the fusedLocationProviderClient to request updates with the current locationRequest and a callback.
+     * It needs to be called everytime the location request changes (e.g. in interval or priority)
+     */
     private void tracking() {
 
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            String[] permissions = {Manifest.permission.INTERNET, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+            requestPermissions(permissions, PERMISSION_REQUEST_CODE);
+        }
+
         fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
-        updateGPS();
+        //updateGPS();
+    }
+    /**
+     * Stops all tracking requests.
+     */
+    private void stopTracking() {
+
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
     }
 
 
-    private void sendApiCall(){
-        ApiCall apiCall = new ApiCall((int) lastKnownPoint.getLatitude(), (int) lastKnownPoint.getLongitude(), (int) Instant.now().getEpochSecond(), activity, result -> {
+    /**
+     * Taking the saved lastPoint, the current time and the set activity, this function calls to the server and relays these information.
+     * It waits for an answer to work with.
+     */
+    private void sendApiCall() {
+        ApiCall apiCall = new ApiCall(lastKnownPoint.getLatitude(), lastKnownPoint.getLongitude(), Instant.now().getEpochSecond(), activity, result -> {
             // Here you girls can do whatever frontend-stuff you want with fieldIndex, for example:
             if (result.fieldIndex != null) {
                 System.out.println(result.fieldIndex + ", " + result.responseCode);
-            } else if (result.responseCode == null ) {
+            } else if (result.responseCode == null) {
                 System.out.println("Cant establish network connection to server :(");
             } else {
                 System.out.println("There are problems with the server." +
